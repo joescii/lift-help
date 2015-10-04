851d6837bc59de644c24c63a9775b56693e3e236
Joe Barnes <barnesjd@gmail.com>
2015-10-04 10:06:34 -0500

    Updating example to show that the split can conditionally match to not affect the rest of the application

--- src/main/scala/code/rest/RestApi.scala
+++ src/main/scala/code/rest/RestApi.scala
@@ -8,10 +8,13 @@ object RestApi extends RestHelper {
   serve {
     case "test" :: filename :: Nil Get _ =>
       PlainTextResponse(s"filename is $filename", 200)
+    case "default" :: filename :: Nil Get _ =>
+      PlainTextResponse(s"filename is $filename", 200)
   }
 
   val splitter:SplitSuffixPF = {
-    case parts => // <= Parts is a List[String] of each part between slashes
+    case parts @ ("test" :: _) => // <= Parts is a List[String] of each part between slashes, matching only if starts with "test"
+      
       // Since you want the entire filename without the suffix split, there is nothing to do
       // but return the parts and an empty suffix
       (parts, "")
