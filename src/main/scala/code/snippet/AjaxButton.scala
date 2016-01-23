32d0bee8402bcc34005b43046d5d27b69070fdf8
Joe Barnes <barnesjd@gmail.com>
2016-01-23 14:38:53 -0600

    Made some stupid little Future example

--- src/main/scala/code/snippet/AjaxButton.scala
+++ src/main/scala/code/snippet/AjaxButton.scala
@@ -1,10 +1,27 @@
 package code.snippet
 
-import net.liftweb.http.SHtml
+import net.liftweb.http.{SessionVar, SHtml}
+import net.liftweb.http.FutureWithSession._
 import net.liftweb.http.js.JE.JsVar
 import net.liftweb.http.js.JsCmds
 import net.liftweb.util.Helpers._
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object MyVar extends SessionVar("init")
+
 object AjaxButton {
-  def render = "type=button [onclick]" #> SHtml.ajaxCall(JsVar("window.blah"), (blah) => {println(blah); JsCmds.Noop})
+  def render = "type=button [onclick]" #>
+    SHtml.ajaxCall(
+      JsVar("window.myGlobal"),
+      myGlobal => {
+        futureOp(myGlobal)
+          .withCurrentSession
+          .foreach(MyVar.set(_))
+        JsCmds.Noop
+      }
+    )
+
+  def futureOp(s:String):Future[String] = Future("Back to the Future[T]: "+s)
 }
