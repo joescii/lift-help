991f3340b560118d35a36ecb5d6e2527a4e28b2f
Joe Barnes <joe@joescii.com>
2017-12-19 15:38:50 -0600

    Created a silly form for adding cookies

---
+++ src/main/scala/code/snippet/CookieForm.scala
@@ -0,0 +1,27 @@
+package code.snippet
+
+import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.provider.HTTPCookie
+import net.liftweb.util.Helpers._
+
+object CookieForm {
+  def render = {
+    var name: String = ""
+    var value: String = ""
+
+    def process(): JsCmd = {
+      val cookie = HTTPCookie(name, value)
+
+      println(s"Adding cookie $cookie")
+      S.addCookie(cookie)
+      println(S.responseCookies)
+      JsRaw(s"console.log('Added cookie: $name = $value')")
+    }
+
+    "name=name" #> SHtml.text(name, name = _) &
+    "name=value" #> (SHtml.text(value, value = _) ++ SHtml.hidden(process))
+  }
+}
